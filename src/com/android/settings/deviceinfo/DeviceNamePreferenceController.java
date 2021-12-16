4a445ec2f1b0dda22866642a15c3552b2e799224
Pranav Vashi <neobuddy89@gmail.com>
2021-12-17 02:51:46 +0530

    Settings: Set device name from marketname if available
    Change-Id: If974ac38638fca9b100008d1b5404d876faf42e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
+++ src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
@@ -23,6 +23,7 @@ import android.net.wifi.SoftApConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.text.SpannedString;
 
@@ -49,6 +50,8 @@ public class DeviceNamePreferenceController extends BasePreferenceController
         LifecycleObserver,
         OnSaveInstanceState,
         OnCreate {
+
+    private static final String KEY_MARKET_NAME_PROP = "ro.product.marketname";
     private static final String KEY_PENDING_DEVICE_NAME = "key_pending_device_name";
     @VisibleForTesting
     static final int RES_SHOW_DEVICE_NAME_BOOL = R.bool.config_show_device_name;
@@ -94,11 +97,11 @@ public class DeviceNamePreferenceController extends BasePreferenceController
     }
 
     private void initializeDeviceName() {
-        mDeviceName = Settings.Global.getString(mContext.getContentResolver(),
+        String deviceName = Settings.Global.getString(mContext.getContentResolver(),
                 Settings.Global.DEVICE_NAME);
-        if (mDeviceName == null) {
-            mDeviceName = Build.MODEL;
-        }
+        if (deviceName == null)
+            deviceName = Build.MODEL;
+        mDeviceName = SystemProperties.get(KEY_MARKET_NAME_PROP, deviceName);
     }
 
     @Override
